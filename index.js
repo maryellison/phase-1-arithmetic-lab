bb75d57186e5f95b0f7b38f8dffa6adb765cc9d4
Mary Ellison Barlow <maryellisonbarlow@gmail.com>
2022-11-09 01:04:34 -0500

    commit tree e44399041463deb9d9cd130693357d8c5e36acf8

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+let num1 = 2
+let num2 = 31
+const multiply = num1 * num2
 
-// Write your code here
+
+//function random(){
+//    return (Math.floor(Math.random() * 10) + 1);
+//}
+let random = Math.floor(Math.random() * 10)
+console.log(random);
+
+let num3 = 16
+let num4 = 4
+
+const mod = num3 / num4
+
+
+const max = Math.max(1, 2, 3, 4, 20)
